6c4147119702a3b18117a5dde58d0634671b13b6
Muh Fernanda Hardiyanto <53845460+MuhFernandaHardiyanto@users.noreply.github.com>
2020-06-24 14:59:44 +0700

    mengembalikan file beranda.dart di beranda1

--- Progres Baru/buswangi/lib/beranda1.dart
+++ Progres Baru/buswangi/lib/beranda1.dart
@@ -130,3 +130,93 @@ class _BottomNavyBarState extends State<BottomNavyBar> {
 
   NavigationItem(this.icon, this.title, this.color);
 }
+
+
+
+////
+// import 'package:buswangi/login.dart';
+// import "package:flutter/material.dart";
+// import 'package:shared_preferences/shared_preferences.dart';
+// import './maps.dart' as maps;
+// import './profile.dart' as profile;
+// import './bus_schedule.dart' as bus_schedule;
+
+
+// class Beranda extends StatefulWidget {
+//   @override
+//   _BerandaState createState() => _BerandaState();
+// }
+
+// class _BerandaState extends State<Beranda> with SingleTickerProviderStateMixin{
+
+//  SharedPreferences sharedPreferences;
+//  TabController controller;
+
+//   @override
+//   void initState() {
+//     controller =
+//         new TabController(vsync: this, length: 3);
+//     super.initState();
+//     checkLoginStatus();
+//   }
+
+//   checkLoginStatus() async {
+//     sharedPreferences = await SharedPreferences.getInstance();
+//     if(sharedPreferences.getString("api_token") == null) {
+//       Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) =>   Login()), (Route<dynamic> route) => false);
+//     }
+//   }
+ 
+// @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: new TabBarView(
+//         controller: controller,
+//         children: <Widget>[
+//           new maps.Maps(),
+//           new bus_schedule.BusSchedule(),
+//           new profile.Profile()
+//         ],
+//       ),
+//       bottomNavigationBar: new Material(
+//         color: Colors.indigo[900],
+//         child: new TabBar(
+//           controller: controller,
+//           tabs: <Widget>[
+//             new Tab(
+//               icon: new Icon(Icons.location_on),
+//             ),
+//             new Tab(
+//               icon: new Icon(Icons.departure_board),
+//             ),
+//             new Tab(
+//               icon: new Icon(Icons.face),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//       }
+// }
+
+// @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Code Land", style: TextStyle(color: Colors.white)),
+//         actions: <Widget>[
+//           FlatButton(
+//             onPressed: () {
+//               sharedPreferences.clear();
+              
+//               Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => Login()), (Route<dynamic> route) => false);
+//             },
+//             child: Text("Log Out", style: TextStyle(color: Colors.white)),
+//           ),
+//         ],
+//       ),
+//       body: Center(child: Text("Main Page")),
+//       drawer: Drawer(),
+//     );
+//   }
+// }
